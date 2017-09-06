488d80799ccd343f30d5eb57e92cb7465acd0478
Fengming Yuan <fmyuan@gmail.com>
2017-09-06 10:35:42 -0400

    Pflotran-Clm-Interface: ALM-PFLOTRAN interface editing for Thermal-Hydrology coupling.

--- components/clm/src/biogeochem/CNEcosystemDynMod.F90
+++ components/clm/src/biogeochem/CNEcosystemDynMod.F90
@@ -560,9 +560,7 @@ contains
 
        call t_startf('CNDecompAlloc')
        !----------------------------------------------------------------
-       if(.not.use_clm_interface) then
-            ! directly run clm-bgc
-            ! if (use_clm_interface & use_clm_bgc), then CNDecomAlloc is called in clm_driver
+       if( .not.use_clm_bgc .and. .not.(use_pflotran .and. pf_cmode) ) then
             call CNDecompAlloc (bounds, num_soilc, filter_soilc,    &
                        num_soilp, filter_soilp,                     &
                        canopystate_vars, soilstate_vars,            &
