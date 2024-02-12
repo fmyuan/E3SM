d9484a7426c38f41274cd8b90e28dc0e46b2263e
Benjamin Sulman <sulmanbn@ornl.gov>
2024-02-12 15:30:26 -0500

    Move alquimia library to shared directory

--- cime_config/machines/cmake_macros/gnu_cades.cmake
+++ cime_config/machines/cmake_macros/gnu_cades.cmake
@@ -1,4 +1,4 @@
 string(APPEND CMAKE_Fortran_FLAGS " -O -fno-range-check")
 set(MPICXX "mpic++")
 set(SCXX "g++")
-set(ALQUIMIA_PATH "$ENV{HOME}/ELM-alquimia/REDOX-PFLOTRAN/alquimia/build/alquimia")
+set(ALQUIMIA_PATH "/nfs/data/ccsi/proj-shared/b0u/ELM-PFLOTRAN/alquimia/")
