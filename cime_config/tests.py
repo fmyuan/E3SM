4cf3ba21f4511805d822214f60ff255167d3abd0
Yuan, Fengming <fmyuan@gmail.com>
2024-03-14 16:49:11 -0400

    Add a few tests for local ELM testing: (1) 5 tests for important ELM modules or functions; (2) 2 debug tests; (3) a long-term test for FLUXNET 42 sites.

--- cime_config/tests.py
+++ cime_config/tests.py
@@ -83,6 +83,28 @@ _TESTS = {
             )
         },
 
+    "e3sm_land_mydebugs" : {
+        "share" : True,
+        "time"  : "0:45:00",
+        "tests" : (
+            "SMS_Ly2_P1x1_D.1x1_smallvilleIA.IELMCNCROP.elm-lulcc_sville",
+            "ERS.ELM_USRDAT.I1850CNPRDCTCBC.elm-mydebug_I1850CNPRDCTCBC"
+            )
+        },
+
+    "e3sm_land_mytests" : {
+        "share" : True,
+        "time"  : "0:45:00",
+        "tests" : (
+            "SMS_Ly2_P1x1.1x1_smallvilleIA.IELMCNCROP.elm-per_crop",
+            "SMS_Ly2_P1x1_D.1x1_smallvilleIA.IELMCNCROP.elm-lulcc_sville",
+            "ERS.ELM_USRDAT.I1850CNPRDCTCBC.elm-usrpft_arctic_I1850CNPRDCTCBC",
+            "ERS.ELM_USRDAT.I1850CNPRDCTCBC.elm-usrpft_onsetgdd_I1850CNPRDCTCBC",
+            "ERS.ELM_USRDAT.I1850CNPRDCTCBC.elm-usrpft_codetest_I1850CNPRDCTCBC",
+            "ERS.ELM_USRDAT.I1850CNPRDCTCBC.elm-usrpft_default_I1850CNPRDCTCBC"
+            )
+        },
+
     "e3sm_land_developer" : {
         "share" : True,
         "time"  : "0:45:00",
@@ -101,6 +123,7 @@ _TESTS = {
             "SMS.r05_r05.IELM.elm-topounit",
             "SMS.r05_r05.IELM.elm-topounit_im2",
             "ERS.ELM_USRDAT.I1850ELM.elm-usrdat",
+            "ERS.ELM_USRDAT.I1850CNPRDCTCBC.elm-usrdat_I1850CNPRDCTCBC42sites",
             "ERS.r05_r05.IELM.elm-lnd_rof_2way",
             "ERS.r05_r05.IELM.elm-V2_ELM_MOSART_features",
             "ERS.ELM_USRDAT.IELM.elm-surface_water_dynamics"
