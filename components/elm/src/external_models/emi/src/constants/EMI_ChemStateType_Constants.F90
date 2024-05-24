b9b01967fe9189f3754b7cae206a73e0f9cca70f
Benjamin Sulman <sulmanbn@ornl.gov>
2024-05-24 10:07:16 -0400

    EMI data exchange for alquimia

--- components/elm/src/external_models/emi/src/constants/EMI_ChemStateType_Constants.F90
+++ components/elm/src/external_models/emi/src/constants/EMI_ChemStateType_Constants.F90
@@ -3,6 +3,39 @@ module EMI_ChemStateType_Constants
   implicit none
   private
   !
-  integer, parameter, public :: L2E_STATE_SOIL_PH  = 1801
+  integer, parameter, public :: L2E_STATE_SOIL_PH                        = 1801
+  integer, parameter, public :: L2E_STATE_WATER_DENSITY                  = 1802
+  integer, parameter, public :: L2E_STATE_AQUEOUS_PRESSURE               = 1803
+  integer, parameter, public :: L2E_STATE_TOTAL_MOBILE                   = 1804
+  integer, parameter, public :: L2E_STATE_TOTAL_IMMOBILE                 = 1805
+  integer, parameter, public :: L2E_STATE_MINERAL_VOLUME_FRACTION        = 1806
+  integer, parameter, public :: L2E_STATE_MINERAL_SPECIFIC_SURFACE_AREA  = 1807
+  integer, parameter, public :: L2E_STATE_SURFACE_SITE_DENSITY           = 1808
+  integer, parameter, public :: L2E_STATE_CATION_EXCHANGE_CAPACITY       = 1809
+  integer, parameter, public :: L2E_STATE_AUX_DOUBLES                    = 1810
+  integer, parameter, public :: L2E_STATE_AUX_INTS                       = 1811
+  integer, parameter, public :: L2E_STATE_FREE_MOBILE                    = 1832
+
+  integer, parameter, public :: E2L_STATE_SOIL_PH                        = 1812
+  integer, parameter, public :: E2L_STATE_SOIL_SALINITY                  = 1813
+  integer, parameter, public :: E2L_STATE_SOIL_O2                        = 1814
+  integer, parameter, public :: E2L_STATE_SOIL_SULFATE                   = 1815
+  integer, parameter, public :: E2L_STATE_SOIL_SULFIDE                   = 1816
+  integer, parameter, public :: E2L_STATE_SOIL_FE2                       = 1817
+  integer, parameter, public :: E2L_STATE_SOIL_FE_OXIDE                  = 1818
+  integer, parameter, public :: E2L_STATE_SOIL_FE_SULFIDE                = 1819
+  integer, parameter, public :: E2L_STATE_SOIL_ACETATE                   = 1820
+  integer, parameter, public :: E2L_STATE_WATER_DENSITY                  = 1821
+  integer, parameter, public :: E2L_STATE_AQUEOUS_PRESSURE               = 1822
+  integer, parameter, public :: E2L_STATE_TOTAL_MOBILE                   = 1823
+  integer, parameter, public :: E2L_STATE_TOTAL_IMMOBILE                 = 1824
+  integer, parameter, public :: E2L_STATE_MINERAL_VOLUME_FRACTION        = 1825
+  integer, parameter, public :: E2L_STATE_MINERAL_SPECIFIC_SURFACE_AREA  = 1826
+  integer, parameter, public :: E2L_STATE_SURFACE_SITE_DENSITY           = 1827
+  integer, parameter, public :: E2L_STATE_CATION_EXCHANGE_CAPACITY       = 1828
+  integer, parameter, public :: E2L_STATE_AUX_DOUBLES                    = 1829
+  integer, parameter, public :: E2L_STATE_AUX_INTS                       = 1830
+  integer, parameter, public :: E2L_STATE_CHEM_DT                        = 1831
+  integer, parameter, public :: E2L_STATE_FREE_MOBILE                    = 1833
 
 end module EMI_ChemStateType_Constants
