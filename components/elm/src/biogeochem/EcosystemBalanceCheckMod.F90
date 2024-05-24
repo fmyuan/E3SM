6389d53996b010339710618442f3f965f66d68dd
Benjamin Sulman <sulmanbn@ornl.gov>
2024-05-24 10:03:01 -0400

    Adding alquimia data storage to ELM data structures and restarts

--- components/elm/src/biogeochem/EcosystemBalanceCheckMod.F90
+++ components/elm/src/biogeochem/EcosystemBalanceCheckMod.F90
@@ -491,8 +491,6 @@ contains
                col_prod1n_loss(c) + col_prod10n_loss(c) + col_prod100n_loss(c)
 
          if(use_alquimia) col_noutputs(c) = col_noutputs(c) + col_nf%DON_runoff(c)
-
-         col_noutputs(c) = col_noutputs(c) + col_prod1n_loss(c)
          
          col_noutputs(c) = col_noutputs(c) - som_n_leached(c)
 
