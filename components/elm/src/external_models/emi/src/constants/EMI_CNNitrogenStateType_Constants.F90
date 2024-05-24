b7fce9eb3d331293408db0caa8ee07494567c441
Benjamin Sulman <sulmanbn@ornl.gov>
2024-05-24 10:07:16 -0400

    EMI data exchange for alquimia

---
+++ components/elm/src/external_models/emi/src/constants/EMI_CNNitrogenStateType_Constants.F90
@@ -0,0 +1,17 @@
+module EMI_CNNitrogenStateType_Constants
+  !
+  implicit none
+  private
+  !
+  integer, parameter, public :: L2E_STATE_NITROGEN_POOLS_VERTICALLY_RESOLVED  = 2101
+  integer, parameter, public :: L2E_STATE_NH4_VERTICALLY_RESOLVED             = 2102
+  integer, parameter, public :: L2E_STATE_NO3_VERTICALLY_RESOLVED             = 2103
+
+  integer, parameter, public :: E2L_STATE_NITROGEN_POOLS_VERTICALLY_RESOLVED  = 2104
+  integer, parameter, public :: E2L_STATE_NH4_VERTICALLY_RESOLVED             = 2105
+  integer, parameter, public :: E2L_STATE_NO3_VERTICALLY_RESOLVED             = 2106
+  integer, parameter, public :: E2L_STATE_DON_VERTICALLY_RESOLVED             = 2107
+  integer, parameter, public :: E2L_STATE_N2O_VERTICALLY_RESOLVED             = 2108
+  integer, parameter, public :: E2L_STATE_N2_VERTICALLY_RESOLVED              = 2109
+
+end module EMI_CNNitrogenStateType_Constants
