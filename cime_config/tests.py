612e8acdd8c1bcda2c4f9ae5d621f8f5e042da28
Fengming Yuan <fmyuan@gmail.com>
2024-05-28 09:16:23 -0400

    2 initial tests on lnd_developer for testing pft physiology on local machine with limited input data, but NOT YET fully work. Two tests are actually similary as 'elm_usrdat' test, but with 42_FLUXNETSITES and a few physiology parameters, based on Sulman et al. (2021).One of them is a short run to test codes, while another is a long run for bug-fixing heavy winter snow caused issue in arctic pft phenology. Those tests won't change simulation results until new codes and parameters are pull in.

--- cime_config/tests.py
+++ cime_config/tests.py
@@ -81,7 +81,9 @@ _TESTS = {
         "tests" : (
             "ERS.ELM_USRDAT.I1850CNPRDCTCBC.elm-usrpft_default_I1850CNPRDCTCBC",
             "ERS.ELM_USRDAT.I1850CNPRDCTCBC.elm-usrpft_codetest_I1850CNPRDCTCBC",
-            "ERS.ELM_USRDAT.I1850CNPRDCTCBC.elm-usrpft_arctic_I1850CNPRDCTCBC"
+            "ERS.ELM_USRDAT.I1850CNPRDCTCBC.elm-usrpft_arctic_I1850CNPRDCTCBC",
+            "ERS.ELM_USRDAT.I1850CNPRDCTCBC.elm-arcticpft_codetest_I1850CNPRDCTCBC",
+            "ERS.ELM_USRDAT.I1850CNPRDCTCBC.elm-arcticpft_onsetgdd_I1850CNPRDCTCBC"
             )
         },
 
@@ -104,6 +106,7 @@ _TESTS = {
             "ERS.ELM_USRDAT.I1850ELM.elm-usrdat",
             "ERS.ELM_USRDAT.I1850CNPRDCTCBC.elm-usrpft_default_I1850CNPRDCTCBC",
             "ERS.ELM_USRDAT.I1850CNPRDCTCBC.elm-usrpft_codetest_I1850CNPRDCTCBC",
+            "ERS.ELM_USRDAT.I1850CNPRDCTCBC.elm-arcticpft_codetest_I1850CNPRDCTCBC",
             "ERS.r05_r05.IELM.elm-lnd_rof_2way",
             "ERS.r05_r05.IELM.elm-V2_ELM_MOSART_features",
             "ERS.ELM_USRDAT.IELM.elm-surface_water_dynamics"
