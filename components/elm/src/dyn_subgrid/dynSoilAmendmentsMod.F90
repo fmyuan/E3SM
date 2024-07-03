2c812f3b8d62d421ce1db1951a0d9e7032de621a
Fengming Yuan <fmyuan@gmail.com>
2024-07-03 10:23:17 -0400

    Add nutrient element data if any when apply soil amendments.

--- components/elm/src/dyn_subgrid/dynSoilAmendmentsMod.F90
+++ components/elm/src/dyn_subgrid/dynSoilAmendmentsMod.F90
@@ -8,6 +8,8 @@ module dynSoilAmendmentsMod
   !    (2) application rate in g/m2/yr, onto each soil patches (pft+cft), of each grid cell.
   !    (3) applied soil amendment is of same quality, grain size and percentage of mixed species, for individual grid cell.
   !        (i.e. NOT patch-level varied like appl. doy and rate)
+  !    (4) applied soil amendment may or may not contain nutrient elements (e.g. N, P, K, ... for plant growth/development).
+  !        and, if any, its weight percentage is part of species.
   
   ! !USES:
 #include "shr_assert.h"
@@ -37,15 +39,18 @@ module dynSoilAmendmentsMod
   type(dyn_var_time_uninterp_type) :: soilamend_doy       ! soil amendment application day of year
   type(dyn_var_time_uninterp_type) :: soilamend_rate      ! soil amendment application rate [g/m2/yr] (yr, pft, lat, lon)
   type(dyn_var_time_uninterp_type) :: soilamend_grainsize ! soil amendment grain size [um] (yr, lat, lon)
-  type(dyn_var_time_uninterp_type) :: soilamend_specpct   ! soil amendment species percentage [-] (yr, species, lat, lon)
+  type(dyn_var_time_uninterp_type) :: soilamend_specpct   ! soil amendment chemical/mineral species weight percentage [-] (yr, species, lat, lon)
+  type(dyn_var_time_uninterp_type) :: soilamend_nutrpct   ! soil amendment nutrient element weight percentage [um] (yr, n-nutrients, lat, lon)
 
   integer :: nspecies          ! species no. of amendment composites
+  integer :: nnutrients        ! nutrient elmement no. of amendment composites (optional)
 
   ! Names of variables on file
   character(len=*), parameter :: soilamend_doy_varname  = 'SOIL_AMENDMENTS_DOY'
   character(len=*), parameter :: soilamend_rate_varname = 'SOIL_AMENDMENTS_RATE'
   character(len=*), parameter :: soilamend_grainsize_varname = 'SOIL_AMENDMENTS_GRAINSIZE'
   character(len=*), parameter :: soilamend_specpct_varname   = 'SOIL_AMENDMENTS_PCT'
+  character(len=*), parameter :: soilamend_nutrpct_varname   = 'SOIL_AMENDMENTS_NUTRIENT_PCT'
 
   character(len=*), parameter, private :: sourcefile = &
        __FILE__
@@ -53,7 +58,7 @@ module dynSoilAmendmentsMod
 
 contains
   !-----------------------------------------------------------------------
-  subroutine dynsoilamendments_init(bounds, dynsoilamendments_filename, namendspec)
+  subroutine dynsoilamendments_init(bounds, dynsoilamendments_filename, namendspec, namendnutr)
     
     ! !DESCRIPTION:
     ! Initialize dataset containing transient crop info (position it to the right time
@@ -68,6 +73,7 @@ contains
     type(bounds_type) , intent(in) :: bounds                     ! proc-level bounds
     character(len=*)  , intent(in) :: dynsoilamendments_filename ! name of file containing soil amendment app information
     integer           , intent(in) :: namendspec                 ! max. number of species in a soil amendment
+    integer,optional  , intent(in) :: namendnutr                 ! max. number of nutrients, e.g. N,P,K, ... in a soil amendment (optional)
     
     ! !LOCAL VARIABLES:
     integer :: num_points          ! number of spatial points
@@ -75,6 +81,7 @@ contains
     integer :: amendappl_shape(3)  ! shape of the amendment application data
     integer :: amendgrain_shape(2)
     integer :: amendspec_shape(3)  ! shape of the amendment species data
+    integer :: amendnutr_shape(3)  ! shape of the amendment nutrients data
     character(len=*), parameter :: subname = 'dynsoilamendments_init'
     !-----------------------------------------------------------------------
     SHR_ASSERT(bounds%level == BOUNDS_LEVEL_PROC, subname // ': argument must be PROC-level bounds')
@@ -93,6 +100,11 @@ contains
     call check_dim(dynsoilamendments_file, 'namendspec', namendspec)
     nspecies = namendspec
 
+    if (present(namendnutr)) then
+        call check_dim(dynsoilamendments_file, 'namendnutr', namendnutr)
+        nnutrients = namendnutr
+    end if
+
     ! read data SOIL_AMENDMENTS_DOY, SOIL_AMENDMENTS_RATE, SOIL_AMENDMENTS_GRAINSIZE, and,
     ! SOIL_AMENDMENTS_PCT corresponding to correct year
     
@@ -100,34 +112,45 @@ contains
 
     amendappl_shape = [num_points, max_topounits, mpft]
     soilamend_doy = dyn_var_time_uninterp_type( &
-         dyn_file = dynsoilamendments_file, varname=soilamend_doy_varname, &
-         dim1name=grlnd, conversion_factor=1._r8, &
-         do_check_sums_equal_1=.false., data_shape=amendappl_shape, &
-         allow_nodata=.true.)
+        dyn_file = dynsoilamendments_file, varname=soilamend_doy_varname, &
+        dim1name=grlnd, conversion_factor=1._r8, &
+        do_check_sums_equal_1=.false., data_shape=amendappl_shape, &
+        allow_nodata=.true.)
     soilamend_rate = dyn_var_time_uninterp_type( &
-         dyn_file = dynsoilamendments_file, varname=soilamend_rate_varname, &
-         dim1name=grlnd, conversion_factor=1._r8, &
-         do_check_sums_equal_1=.false., data_shape=amendappl_shape, &
-         allow_nodata=.true.)
+        dyn_file = dynsoilamendments_file, varname=soilamend_rate_varname, &
+        dim1name=grlnd, conversion_factor=1._r8, &
+        do_check_sums_equal_1=.false., data_shape=amendappl_shape, &
+        allow_nodata=.true.)
 
     amendgrain_shape = [num_points, max_topounits]
     soilamend_grainsize = dyn_var_time_uninterp_type( &
-         dyn_file = dynsoilamendments_file, varname=soilamend_grainsize_varname, &
-         dim1name=grlnd, conversion_factor=1._r8, &
-         do_check_sums_equal_1=.false., data_shape=amendgrain_shape, &
-         allow_nodata=.true.)
+        dyn_file = dynsoilamendments_file, varname=soilamend_grainsize_varname, &
+        dim1name=grlnd, conversion_factor=1._r8, &
+        do_check_sums_equal_1=.false., data_shape=amendgrain_shape, &
+        allow_nodata=.true.)
 
     amendspec_shape = [num_points, max_topounits, namendspec]
+    ! not necessary added up to 100%, but must less than 100% (will do check on-fly)
     soilamend_specpct = dyn_var_time_uninterp_type( &
-         dyn_file = dynsoilamendments_file, varname=soilamend_specpct_varname, &
-         dim1name=grlnd, conversion_factor=100._r8, &
-         do_check_sums_equal_1=.true., data_shape=amendspec_shape, &
-         allow_nodata=.true.)
+        dyn_file = dynsoilamendments_file, varname=soilamend_specpct_varname, &
+        dim1name=grlnd, conversion_factor=100._r8, &
+        do_check_sums_equal_1=.false., data_shape=amendspec_shape, &
+        allow_nodata=.true.)
+
+    if (present(namendnutr)) then
+      amendnutr_shape = [num_points, max_topounits, namendnutr]
+      ! not necessary counted separatedly or added up to 100%, but must less than 100% (will do check on-fly)
+      soilamend_nutrpct = dyn_var_time_uninterp_type( &
+        dyn_file = dynsoilamendments_file, varname=soilamend_nutrpct_varname, &
+        dim1name=grlnd, conversion_factor=100._r8, &
+        do_check_sums_equal_1=.false., data_shape=amendnutr_shape, &
+        allow_nodata=.true.)
+    end if
 
   end subroutine dynsoilamendments_init
 
   !-----------------------------------------------------------------------
-  subroutine dynsoilamendments_appl(bounds, appl_rate, appl_grainsize, appl_specpct)
+  subroutine dynsoilamendments_appl(bounds, appl_rate, appl_grainsize, appl_specpct, appl_nutrpct)
 
     ! !DESCRIPTION:
     ! Get soil amendment application for model time exactly (no temporal interp), when needed.
@@ -143,9 +166,10 @@ contains
 
     ! !ARGUMENTS:
     type(bounds_type), intent(in) :: bounds  ! proc-level bounds
-    real(r8), pointer, intent(out):: appl_rate(:)      ! col-level
-    real(r8), pointer, intent(out):: appl_grainsize(:) ! col-level
-    real(r8), pointer, intent(out):: appl_specpct(:,:) ! col-level
+    real(r8), pointer, intent(out):: appl_rate(:)                 ! col-level, summed from patches
+    real(r8), pointer, intent(out):: appl_grainsize(:,:)          ! col-level, summed from patches
+    real(r8), pointer, intent(out):: appl_specpct(:,:)            ! col-level, summed from patches
+    real(r8), pointer, optional,intent(out):: appl_nutrpct(:,:)   ! col-level, summed from patches
 
     ! !LOCAL VARIABLES:
     integer               :: m,p,c,l,g,t,t2,ti,topi      ! indices
@@ -153,6 +177,7 @@ contains
     real(r8), allocatable :: appl_rate_cur(:,:,:)        ! current timestep appl rate
     real(r8), allocatable :: appl_grainsize_cur(:,:)     ! current timestep appl grain size
     real(r8), allocatable :: appl_specpct_cur(:,:,:)     ! current timestep appl specpct
+    real(r8), allocatable :: appl_nutrpct_cur(:,:,:)     ! current timestep appl nutrpct
     character(len=*), parameter :: subname = 'dynsoilamendments_appl'
     !-----------------------------------------------------------------------
 
@@ -162,6 +187,8 @@ contains
     allocate(appl_rate_cur(bounds%begg:bounds%endg, max_topounits, natpft_lb:cft_ub))
     allocate(appl_grainsize_cur(bounds%begg:bounds%endg, max_topounits))
     allocate(appl_specpct_cur(bounds%begg:bounds%endg, max_topounits, nspecies))
+    if (present(appl_nutrpct)) &
+      allocate(appl_specpct_cur(bounds%begg:bounds%endg, max_topounits, nspecies))
 
     ! note here data obtained is instant (not averaged over whole year)
     ! Keep in mind that what needed is an instant value for each variable
@@ -169,11 +196,15 @@ contains
     call soilamend_rate%get_current_data(appl_rate_cur)          ! patch-level
     call soilamend_grainsize%get_current_data(appl_grainsize_cur)! grid-level
     call soilamend_specpct%get_current_data(appl_specpct_cur)    ! grid-level
+    if (present(appl_nutrpct)) &
+      call soilamend_nutrpct%get_current_data(appl_nutrpct_cur)    ! grid-level
 
     ! output vars (already allocated memory, but may not zeroed)
-    appl_rate(bounds%begc:bounds%endc)       = 0._r8
-    appl_grainsize(bounds%begc:bounds%endc)  = 1._r8 ! non-zero to avoid math issue. it's ok as long as rate is 0
-    appl_specpct(bounds%begc:bounds%endc, :) = 0._r8
+    appl_rate(bounds%begc:bounds%endc)        = 0._r8
+    appl_grainsize(bounds%begc:bounds%endc,:) = 1._r8 ! non-zero to avoid math issue. it's ok as long as rate is 0
+    appl_specpct(bounds%begc:bounds%endc, :)  = 0._r8
+    if (present(appl_nutrpct)) &
+      appl_nutrpct(bounds%begc:bounds%endc, :)  = 0._r8
     do p = bounds%begp, bounds%endp
        g = veg_pp%gridcell(p)
        l = veg_pp%landunit(p)
@@ -193,8 +224,14 @@ contains
              ! So if really want to patch-level, must only allow one patch per column.
              appl_rate(c) = appl_rate(c) + appl_rate_cur(g,ti,m) * veg_pp%wtcol(p)
 
-             appl_grainsize(c) = appl_grainsize_cur(g,ti)
+             ! assuming grain size is same for its mineral components at application
+             ! but which may be changeable during dissolution and upon mineral type
+             appl_grainsize(c,:) = appl_grainsize_cur(g,ti)
+
              appl_specpct(c,:) = appl_specpct_cur(g,ti,:)
+
+             if (present(appl_nutrpct)) &
+               appl_nutrpct(c,:) = appl_nutrpct_cur(g,ti,:)
 
           end if
        end if
@@ -204,6 +241,7 @@ contains
     deallocate(appl_rate_cur)
     deallocate(appl_grainsize_cur)
     deallocate(appl_specpct_cur)
+    deallocate(appl_nutrpct_cur)
 
   end subroutine dynsoilamendments_appl
 
