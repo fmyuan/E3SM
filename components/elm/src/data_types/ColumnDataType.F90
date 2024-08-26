2d5cb7775573e25bbb8ca6ef609d761b24eb0135
Benjamin Sulman <sulmanbn@ornl.gov>
2024-08-26 16:47:56 -0400

    Add DOC, DIC, SIC to restart files

--- components/elm/src/data_types/ColumnDataType.F90
+++ components/elm/src/data_types/ColumnDataType.F90
@@ -2837,6 +2837,23 @@ contains
             dim1name='column', long_name='', units='', &
             interpinic_flag='interp', readvar=readvar, data=this%prod1c)
 
+       if (use_alquimia) then
+         call restartvar(ncid=ncid, flag=flag, varname='DIC_vr', xtype=ncd_double,  &
+               dim1name='column', dim2name='levgrnd', switchdim=.true., &
+               long_name='',  units='', fill_value=0.0_r8, &
+               interpinic_flag='interp', readvar=readvar, data=this%DIC_vr)
+
+         call restartvar(ncid=ncid, flag=flag, varname='DOC_vr', xtype=ncd_double,  &
+               dim1name='column', dim2name='levgrnd', switchdim=.true., &
+               long_name='',  units='', fill_value=0.0_r8, &
+               interpinic_flag='interp', readvar=readvar, data=this%DOC_vr)
+
+         call restartvar(ncid=ncid, flag=flag, varname='SIC_vr', xtype=ncd_double,  &
+               dim1name='column', dim2name='levgrnd', switchdim=.true., &
+               long_name='',  units='', fill_value=0.0_r8, &
+               interpinic_flag='interp', readvar=readvar, data=this%SIC_vr)
+       endif
+
     end if ! C12
 
     if ( carbon_type == 'c13' ) then
@@ -3366,7 +3383,7 @@ contains
             this%totSIC(c) + &
             this%ctrunc(c)   + &
             this%cropseedc_deficit(c)
-
+      write(iulog,*),this%totcolc(c),this%totDIC(c),this%totDOC(c)
        this%totabgc(c) =       &
             this%totprodc(c) + &
             this%seedc(c)    + &
