7364f19a71179a3b337aa0ce20b49d574b7d3242
Fengming Yuan <fmyuan@gmail.com>
2024-10-01 11:00:49 -0400

    A bug in column N balance checking.

--- components/elm/src/biogeochem/EcosystemBalanceCheckMod.F90
+++ components/elm/src/biogeochem/EcosystemBalanceCheckMod.F90
@@ -491,8 +491,6 @@ contains
 
          if(use_alquimia) col_noutputs(c) = col_noutputs(c) + col_nf%DON_runoff(c)
 
-         col_noutputs(c) = col_noutputs(c) + col_prod1n_loss(c)
-         
          col_noutputs(c) = col_noutputs(c) - som_n_leached(c)
 
          if (use_fan) col_noutputs(c) = col_noutputs(c) + fan_totnout(c)
