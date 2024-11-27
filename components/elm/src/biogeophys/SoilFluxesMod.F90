fa9f9a6a1c318fffae3b8e1be0c8a6634c35077f
Fengming Yuan <fmyuan@gmail.com>
2024-11-27 17:56:54 -0500

    gfortran built from clang 15 and above, may have issues of power operator (**), especially when in DEBUG mode on.

--- components/elm/src/biogeophys/SoilFluxesMod.F90
+++ components/elm/src/biogeophys/SoilFluxesMod.F90
@@ -279,13 +279,13 @@ contains
          ! Ground heat flux
 
          if (.not. lun_pp%urbpoi(l)) then
-            lw_grnd=(frac_sno_eff(c)*tssbef(c,col_pp%snl(c)+1)**4 &
-                 +(1._r8-frac_sno_eff(c)-frac_h2osfc(c))*tssbef(c,1)**4 &
-                 +frac_h2osfc(c)*t_h2osfc_bef(c)**4)
+            lw_grnd=(frac_sno_eff(c)*tssbef(c,col_pp%snl(c)+1)**4_r8 &
+                 +(1._r8-frac_sno_eff(c)-frac_h2osfc(c))*tssbef(c,1)**4_r8 &
+                 +frac_h2osfc(c)*t_h2osfc_bef(c)**4_r8)
 
             eflx_soil_grnd(p) = ((1._r8- frac_sno_eff(c))*sabg_soil(p) + frac_sno_eff(c)*sabg_snow(p)) + dlrad(p) &
                  + (1-frac_veg_nosno(p))*emg(c)*forc_lwrad(t) &
-                 - emg(c)*sb*lw_grnd - emg(c)*sb*t_grnd0(c)**3*(4._r8*tinc(c)) &
+                 - emg(c)*sb*lw_grnd - emg(c)*sb*t_grnd0(c)**3_r8*(4._r8*tinc(c)) &
                  - (eflx_sh_grnd(p)+qflx_evap_soi(p)*htvp(c))
 
             if (lun_pp%itype(l) == istsoil .or. lun_pp%itype(l) == istcrop) then
@@ -296,7 +296,7 @@ contains
             ! the term (emg*sb*tssbef(col_pp%snl+1)**4) is not the upward longwave flux because of
             ! interactions between urban columns.
 
-            eflx_lwrad_del(p) = 4._r8*emg(c)*sb*t_grnd0(c)**3*tinc(c)
+            eflx_lwrad_del(p) = 4._r8*emg(c)*sb*t_grnd0(c)**3_r8*tinc(c)
 
             ! Include transpiration term because needed for pervious road
             ! and wasteheat and traffic flux
@@ -422,14 +422,14 @@ contains
          j = col_pp%snl(c)+1
 
          if (.not. lun_pp%urbpoi(l)) then
-            lw_grnd=(frac_sno_eff(c)*tssbef(c,col_pp%snl(c)+1)**4 &
-                 +(1._r8-frac_sno_eff(c)-frac_h2osfc(c))*tssbef(c,1)**4 &
-                 +frac_h2osfc(c)*t_h2osfc_bef(c)**4)
+            lw_grnd=(frac_sno_eff(c)*tssbef(c,col_pp%snl(c)+1)**4_r8 &
+                 +(1._r8-frac_sno_eff(c)-frac_h2osfc(c))*tssbef(c,1)**4_r8 &
+                 +frac_h2osfc(c)*t_h2osfc_bef(c)**4_r8)
 
             eflx_lwrad_out(p) = ulrad(p) &
                  + (1-frac_veg_nosno(p))*(1.-emg(c))*forc_lwrad(t) &
                  + (1-frac_veg_nosno(p))*emg(c)*sb*lw_grnd &
-                 + 4._r8*emg(c)*sb*t_grnd0(c)**3*tinc(c)
+                 + 4._r8*emg(c)*sb*t_grnd0(c)**3_r8*tinc(c)
 
             eflx_lwrad_net(p) = eflx_lwrad_out(p) - forc_lwrad(t)
             if (lun_pp%itype(l) == istsoil .or. lun_pp%itype(l) == istcrop) then
