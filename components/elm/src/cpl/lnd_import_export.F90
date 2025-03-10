3ea3504e2d2bfa024c38e8dfbb9d574c4ff0a4ed
Fengming Yuan <fmyuan@gmail.com>
2025-03-10 16:00:55 -0400

    CPL_BYPASS modification (12): Allow cpl_bypass module workable for forcing data derived from CRUJRA5 trendy.

--- components/elm/src/cpl/lnd_import_export.F90
+++ components/elm/src/cpl/lnd_import_export.F90
@@ -425,7 +425,15 @@ contains
                     !metdata_fname = 'WCYCL1850S.ne30_' // trim(metvars(v)) // '_0076-0100_z' // zst(2:3) // '.nc'
                     metdata_fname = 'CBGC1850S.ne30_' // trim(metvars(v)) // '_0566-0590_z' // zst(2:3) // '.nc'
             else if (atm2lnd_vars%metsource == 6) then
-                metdata_fname = 'ERA5_' // trim(metvars(v)) // '_1950-2025_z' // zst(2:3) // '.nc'
+                ! CRUJRA v2.3
+                metdata_fname = 'CRUJRAV2.3.c2023.0.5x0.5_' // trim(metvars(v)) // '_1901-2021_z' // zst(2:3) // '.nc'
+                if(index(metdata_type, 'trendy2025') .gt. 0) then
+                    metdata_fname = 'elmforc.TRENDY.c2025.0.5x0.5_' // trim(metvars(v)) // '_1901-2024_z' // zst(2:3) // '.nc'
+                elseif(index(metdata_type, 'trendy2024') .gt. 0) then
+                    metdata_fname = 'crujra.v2.5.5d.0.5x0.5_' // trim(metvars(v)) // '_1901-2023_z' // zst(2:3) // '.nc'
+                elseif(index(metdata_type, 'trendy2023') .gt. 0) then
+                    metdata_fname = 'crujra.v2.4.5d.0.5x0.5_' // trim(metvars(v)) // '_1901-2022_z' // zst(2:3) // '.nc'
+                endif
             else if (atm2lnd_vars%metsource == 7) then
                 ! ERA5
                 if (use_daymet .and. (index(metdata_type, 'daymet4') .gt. 0) ) then
