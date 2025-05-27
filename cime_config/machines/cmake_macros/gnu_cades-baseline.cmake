cfbd087d326de0593c3783ee0fa51a0d6d3f4c56
Benjamin Sulman <sulmanbn@ornl.gov>
2025-05-27 13:03:07 -0800

    Bsulman/lnd/alquimia baseline fix (#16)
    Change alquimia location to shared dir

--- cime_config/machines/cmake_macros/gnu_cades-baseline.cmake
+++ cime_config/machines/cmake_macros/gnu_cades-baseline.cmake
@@ -17,4 +17,6 @@ string(APPEND SLIBS " -L$ENV{PNETCDF_PATH}/lib -lpnetcdf -L$ENV{HDF5_PATH}/lib -
 string(APPEND SLIBS " -L${BLASLAPACK_LIBDIR} -lblas -llapack")
 string(APPEND CXX_LIBS " -lstdc++")
 set(PIO_FILESYSTEM_HINTS "gpfs")
-set(ALQUIMIA_PATH "/ccsopen/home/b0u/ELM-alquimia/REDOX-PFLOTRAN/alquimia/build/alquimia")
+set(ALQUIMIA_PATH "/ccsopen/proj/cli185/b0u/alquimia")
+set(ALQUIMIA_INC "/ccsopen/proj/cli185/b0u/alquimia/include/alquimia")
+set(ALQUIMIA_LIB "/ccsopen/proj/cli185/b0u/alquimia/lib")
