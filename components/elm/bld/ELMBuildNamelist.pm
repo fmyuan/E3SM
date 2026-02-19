e9c363ba8dc66263e2a09efa92435a8b7727e324
Fengming Yuan <fmyuan@gmail.com>
2026-02-19 13:14:37 -0500

    add an ELM fates namlist, use_planthydro_salinity. Note it's only true when use_fates and use_planthydro true.
    (cherry picked from commit a0221f9b5b5d349e1ef29898d992cd6fb15a82c0)
    (cherry picked from commit 2cc56563f4e07237efd77115025f1dead1436342)
    (cherry picked from commit 52f23585882d3c0a62ffc4cbc8d4e03fc3b7a697)

--- components/elm/bld/ELMBuildNamelist.pm
+++ components/elm/bld/ELMBuildNamelist.pm
@@ -825,6 +825,7 @@ sub setup_cmdl_fates_mode {
                      "use_fates_lupft",
                      "use_fates_nocomp",
                      "use_fates_planthydro",
+                     "use_fates_planthydro_salinity",
                      "use_fates_potentialveg",
                      "use_fates_sp",
                      "use_fates_tree_damage",
@@ -888,6 +889,10 @@ sub setup_cmdl_fates_mode {
 	   fatal_error("$var is being set, but can ONLY be set when -bgc fates option is used.\n");
        }
        $var = "use_fates_planthydro";
+       if ( defined($nl->get_value($var)) ) {
+	   fatal_error("$var is being set, but can ONLY be set when -bgc fates option is used.\n");
+       }
+       $var = "use_fates_planthydro_salinity";
        if ( defined($nl->get_value($var)) ) {
 	   fatal_error("$var is being set, but can ONLY be set when -bgc fates option is used.\n");
        }
@@ -3441,6 +3446,7 @@ sub setup_logic_fates {
                    "use_fates_inventory_init",
                    "use_fates_lupft",
                    "use_fates_planthydro",
+                   "use_fates_planthydro_salinity",
                    "use_fates_potentialveg",
                    "use_fates_sp",
                    "use_fates_tree_damage",
@@ -3495,6 +3501,9 @@ sub setup_logic_fates {
           }
           # hydro isn't currently supported to work when FATES SP mode is active
           if (&value_is_true( $nl->get_value('use_fates_planthydro') )) {
+                fatal_error('fates sp mode is currently not supported to work with fates hydro');
+          }
+          if (&value_is_true( $nl->get_value('use_fates_planthydro_salinity') )) {
                 fatal_error('fates sp mode is currently not supported to work with fates hydro');
           }
           # FATES landuse can not be active with fates sp mode is active
