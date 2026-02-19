190edfa74b7342be0bc88ed3edbdd678bf212f7c
Fengming Yuan <fmyuan@gmail.com>
2026-02-19 13:14:37 -0500

    add an ELM fates namlist, use_planthydro_salinity. Note it's only true when use_fates and use_planthydro true.
    (cherry picked from commit a0221f9b5b5d349e1ef29898d992cd6fb15a82c0)
    (cherry picked from commit 2cc56563f4e07237efd77115025f1dead1436342)
    (cherry picked from commit 52f23585882d3c0a62ffc4cbc8d4e03fc3b7a697)

--- components/elm/src/main/controlMod.F90
+++ components/elm/src/main/controlMod.F90
@@ -109,6 +109,7 @@ module controlMod
                         alquimia_NH4_name,  alquimia_NO3_name,    &
                         alquimia_handsoff
   use elm_varctl              , only: tide_file
+  use elm_varctl              , only: use_fates_planthydro_salinity
 
   !
   ! !PUBLIC TYPES:
@@ -320,6 +321,7 @@ contains
           fates_spitfire_mode,                          &
           fates_harvest_mode,                           &
           use_fates_planthydro,                         &
+          use_fates_planthydro_salinity,                &
           use_fates_ed_st3,                             &
           use_fates_cohort_age_tracking,                &
           use_fates_ed_prescribed_phys,                 &
@@ -931,6 +933,7 @@ contains
     call mpi_bcast (fluh_timeseries, len(fluh_timeseries) , MPI_CHARACTER, 0, mpicom, ier)
     call mpi_bcast (flandusepftdat, len(flandusepftdat) , MPI_CHARACTER, 0, mpicom, ier)
     call mpi_bcast (use_fates_planthydro, 1, MPI_LOGICAL, 0, mpicom, ier)
+    call mpi_bcast (use_fates_planthydro_salinity, 1, MPI_LOGICAL, 0, mpicom, ier)
     call mpi_bcast (use_fates_cohort_age_tracking, 1, MPI_LOGICAL, 0, mpicom, ier)
     call mpi_bcast (use_fates_ed_st3, 1, MPI_LOGICAL, 0, mpicom, ier)
     call mpi_bcast (use_fates_fixed_biogeog, 1, MPI_LOGICAL, 0, mpicom, ier)
@@ -1402,6 +1405,7 @@ contains
        write(iulog, *) '    fluh_timeseries = ', trim(fluh_timeseries)
        write(iulog, *) '    flandusepftdat = ', trim(flandusepftdat)
        write(iulog, *) '    use_fates_planthydro = ', use_fates_planthydro
+       write(iulog, *) '    use_fates_planthydro_salinity = ', use_fates_planthydro_salinity
        write(iulog, *) '    use_fates_tree_damage = ', use_fates_tree_damage
        write(iulog, *) '    use_fates_cohort_age_tracking = ',use_fates_cohort_age_tracking
        write(iulog, *) '    fates_parteh_mode = ', fates_parteh_mode
